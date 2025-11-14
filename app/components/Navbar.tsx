f31c984d3d8e1fc3335885a97848efbc77d5cfdf
Lindex Godwinz <malindegodwin9@gmail.com>
2025-11-14 22:16:37 +0300

    initial commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link className="" to="/">
+                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+            </Link>
+        </nav>
+    )
+}
+export default Navbar
