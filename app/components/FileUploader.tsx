2fbdd4037a9a689e99124f2e6c58943faf533fd7
Godwin Malinde <malindegodwin9@gmail.com>
2025-11-24 18:19:35 +0300

    initial commit

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -11,7 +11,7 @@ const FileUploader = ({onFileSelect}: FileUploaderProps) => {
 
     const onDrop = useCallback((acceptedFiles:File[]) => {
         // Do something with the files
-        const file:File = acceptedFiles[0] || null;
+        const file = acceptedFiles[0] || null;
 
         onFileSelect?.(file);
     }, [onFileSelect]);
@@ -32,30 +32,33 @@ const FileUploader = ({onFileSelect}: FileUploaderProps) => {
             <div {...getRootProps()}>
                 <input {...getInputProps()} />
                <div className="space-y-4 cursor-pointer">
-                   <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
-                       <img src ="/icons/info.svg" alt="upload" className="size-20" />
-                   </div>
                    {
                        file ? (
-                           <div className="flex items-center space-x-3">
-                               <img src="/images/pdf.png" alt="upload" className="size-10" />
-                               <div>
-                                   <p className="text-lg text-gray-700 font-medium truncate">
-                                       {file.name}
-                                   </p>
-                                   <p className="text-sm text-gray-500">
-                                       {formatSize(file.size)}
-                                   </p>
-                               </div>
-                               <button className="p-2 cursor-pointer" onClick={(e) =>{
-                                   onFileSelect?.(null);
-                               }}>
+                           <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
+                               <img src="/images/pdf.png" alt="pdf" className="size-10" />
+                               <div className="flex items-center space-x-3">
+                                   <div>
+                                       <p className="text-sm text-gray-700 font-medium truncate max-w-xs">
+                                           {file.name}
+                                       </p>
+                                       <p className="text-sm text-gray-500">
+                                           {formatSize(file.size)}
+                                       </p>
+                                   </div>
+                                   <button className="p-2 cursor-pointer" onClick={(e) =>{
+                                       onFileSelect?.(null);
+                                   }}>
 
-                                   <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" />
-                               </button>
+                                       <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" />
+                                   </button>
+                               </div>
                            </div>
+
                        ) : (
                            <div>
+                               <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                                   <img src ="/icons/info.svg" alt="upload" className="size-20" />
+                               </div>
                                <p className="text-lg text-gray-500">
                                    <span className="font-semibold">
                                        Click to upload
